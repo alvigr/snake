d4a24551ea282a41d72def2b5e11ab39118eff86
alvigr <44508235+alvigr@users.noreply.github.com>
2019-07-23 21:03:32 +0300

    Подключаем web-socket

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/online.html');
+});
+
+app.get('/stream', (req, res) => {
+  //res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/stream.html');
+});
+
+
+io.on('connection', client => {
+  console.log('a user connected');
+  let snakeLevel = 0
+  setInterval(() => {
+    client.emit('event', { snakeLevel: snakeLevel++ })
+  }, 2000)
+  // client.on('event', data => { 
+  //   console.log('event', data)
+  //  });
+  client.on('disconnect', () => { 
+    console.log('user closed connection')
+   });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
