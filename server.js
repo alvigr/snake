d5ddc04c5cb3b3ea5afedca29aa49bd4c98c0d17
alvigr <44508235+alvigr@users.noreply.github.com>
2019-08-20 20:53:02 +0300

    Порт можно настраивать

--- server.js
+++ server.js
@@ -71,8 +71,10 @@ io.on('connection', client => {
    })
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
 
 function addPlayer (game, player) {
